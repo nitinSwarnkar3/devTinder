e027c7579dacd14b566bb7c22b9224915fb3564d
nitinSwarnkar <nitinswarnkar332000@gmail.com>
2025-02-22 13:04:19 +0530

    Middlewares Concepts

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Namaste!!");
+//Middleware concepts
+
+app.use("/admin", adminAuth);
+
+
+
+app.get("/user/getData", userAuth, (req, res) => {
+    res.send("User data sent!");
+})
+
+app.get("/admin/getData", (req,res) => {
+    res.send("All data Sent!");
 });
 
-app.get("/home", (req, res) => {
-    res.send("Namaste from Home");
-});
+app.get("/admin/deleteData", (req,res) =>{
+    res.send("Data deleted successfully!");
+})
+
+
 
 app.listen("3000", () => {
     console.log("Server is connected successfully at port no. 3000")
@@ -47,22 +60,5 @@ app.listen("3000", () => {
 
 
 
-// const express = require("express");
 
-// const app = express();
 
-// app.use("/", (req, res) => {
-//     res.send("Namaste !!");
-// });
-
-// app.use("/home", (req, res) => {
-//     res.send("Namaste from the Home!!");
-// });
-
-// app.use("/login", (req, res) => {
-//     res.send("Namaste from the Login!!");
-// });
-
-// app.listen(3000, () => {
-//     console.log("Server is successfully listening on port no. 3000");
-// });
